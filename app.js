0d51ac9dee1fa3a2aa1a3b34f1b27ece1defe379
Amaan Shaikh <shaikhamaan257@gmail.com>
2017-12-01 04:37:36 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It works');
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server started at port ${port}...`);
+});
