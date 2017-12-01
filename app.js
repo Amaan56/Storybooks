bdff565bdf889162343330fc2697b79cbb26f5ac
Amaan Shaikh <shaikhamaan257@gmail.com>
2017-12-01 06:45:53 +0530

    Auth Added

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+
+//Passport config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
 
 app.get('/', (req, res) => {
   res.send('It works');
 });
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:
+    '250756774522-th9ei4218ept2ulmvs3fldl69pmml8n3.apps.googleusercontent.com',
+  googleClientSecret: '5iFN37llfF9ENm3QSS5vuMtl',
+  mongoURI: 'mongodb://Amaan:password@ds125556.mlab.com:25556/storybooks-dev'
+};
